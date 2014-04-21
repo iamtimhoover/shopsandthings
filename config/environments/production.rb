4bcdae86eb86bc91c43685b06606f57d0ca4591e
Tim Hoover <timothyhoov@gmail.com>
2014-04-21 12:27:27 -0400

    styling for mobile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,14 @@ Rails.application.configure do
 
   # Required for Devise // Note: set this to actual host.
   config.action_mailer.default_url_options = { :host => 'shopsandthings.herokuapp.com' }
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
